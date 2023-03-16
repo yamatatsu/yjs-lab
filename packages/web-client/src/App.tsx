3329e94948eb5569c5ce79fa9347e15b36d0dfb8
yamatatsu <the.kinnikuman@gmail.com>
2023-03-16 09:50:34 +0900

    feat(web-client): monaco editor

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -1,7 +1,8 @@
 import * as Y from "yjs";
 import { useEffect, useState } from "react";
 import { WebsocketProvider } from "y-websocket-for-aws-apigateway";
-import "./App.css";
+import { MonacoBinding } from "y-monaco";
+import Editor from "@monaco-editor/react";
 
 function App() {
   const [mapKey, setMapKey] = useState("");
@@ -23,6 +24,8 @@ function App() {
   return (
     <div className="App">
       <h1>YJS Demo App</h1>
+
+      <h2>Map</h2>
       <div>
         <input value={mapKey} onChange={(e) => setMapKey(e.target.value)} />
         <input value={mapVal} onChange={(e) => setMapVal(e.target.value)} />
@@ -36,7 +39,8 @@ function App() {
         <button onClick={redoMap}>redo</button>
       </div>
       <div>{JSON.stringify(mapJson, null, 2)}</div>
-      <line></line>
+
+      <h2>Array</h2>
       <div>
         <input value={arrVal} onChange={(e) => setArrVal(e.target.value)} />
       </div>
@@ -49,6 +53,29 @@ function App() {
         <button onClick={redoArr}>redo</button>
       </div>
       <div>{JSON.stringify(arrJson, null, 2)}</div>
+
+      <h2>Text</h2>
+      <Editor
+        height="300px"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        options={{
+          minimap: { enabled: false },
+          lineNumbers: "off",
+        }}
+        onMount={(editor, monaco) => {
+          const model = editor.getModel();
+          if (!model) {
+            throw new Error("No model is mounted");
+          }
+          const monacoBinding = new MonacoBinding(
+            txt,
+            model,
+            new Set([editor]),
+            wsProvider.awareness
+          );
+        }}
+      />
     </div>
   );
 }
@@ -59,13 +86,14 @@ const doc = new Y.Doc();
 const url = "wss://ijxmnnebka.execute-api.ap-northeast-1.amazonaws.com/dev/";
 const token = import.meta.env.VITE_WEBSOCKET_TOKEN;
 const docId = "web-client-doc";
-new WebsocketProvider(url, docId, doc, {
+const wsProvider = new WebsocketProvider(url, docId, doc, {
   subprotocols: [token],
   disableBc: true,
 });
 
 const map = doc.getMap<string>("my-map");
 const arr = doc.getArray<string>("my-array");
+const txt = doc.getText("my-text");
 const mapUndoManager = new Y.UndoManager(map);
 const arrUndoManager = new Y.UndoManager(arr);
 
